f524d0fa13438e042702961959f3c13b834a51ec
KestutisRuockus <kestutisruockus@gmail.com>
2023-12-02 23:49:14 +0200

    quiz completed

--- quiz.js
+++ quiz.js
@@ -2,26 +2,31 @@ let questionAnswerNr1,
   questionAnswerNr2,
   questionAnswerNr3,
   questionAnswerNr4,
-  questionAnswerNr5,
-  correctAnswerNr1 = "paris",
-  correctAnswerNr2 = "seven",
-  correctAnswerNr3 = "mars",
-  correctAnswerNr4 = "carbon-dioxide",
-  correctAnswerNr5 = "blue-whale";
+  questionAnswerNr5;
+let correctAnswersList = [
+  "paris",
+  "seven",
+  "mars",
+  "carbon-dioxide",
+  "blue-whale",
+];
+// elements of hidden answer list
+let correctAnswerElement = document.querySelectorAll(".correct-answer");
 
-// get selected answer id
+// get selected item id
+// event - triggered event
 function getId(event) {
   let id = event.id;
   styleSelectedAnswer(id);
 }
 
 // mark selected answer
+// id - selected answer id
 function styleSelectedAnswer(id) {
   let element = document.querySelector(`#${id}`);
   // question 1
   if (id === "berlin" || id === "london" || id === "paris") {
     questionAnswerNr1 = id;
-    console.log(`Question 1 answer: ${questionAnswerNr1}`); // selected answer
     document.querySelector("#berlin").classList.remove("selected");
     document.querySelector("#london").classList.remove("selected");
     document.querySelector("#paris").classList.remove("selected");
@@ -30,7 +35,6 @@ function styleSelectedAnswer(id) {
   // question 2
   else if (id === "five" || id === "seven" || id === "six") {
     questionAnswerNr2 = id;
-    console.log(`Question 2 answer: ${questionAnswerNr2}`); // selected answer
     document.querySelector("#five").classList.remove("selected");
     document.querySelector("#seven").classList.remove("selected");
     document.querySelector("#six").classList.remove("selected");
@@ -39,7 +43,6 @@ function styleSelectedAnswer(id) {
   // question 3
   else if (id === "mars" || id === "jupiter" || id === "saturn") {
     questionAnswerNr3 = id;
-    console.log(`Question 3 answer: ${questionAnswerNr3}`); // selected answer
     document.querySelector("#mars").classList.remove("selected");
     document.querySelector("#jupiter").classList.remove("selected");
     document.querySelector("#saturn").classList.remove("selected");
@@ -48,7 +51,6 @@ function styleSelectedAnswer(id) {
   // question 4
   else if (id === "oxygen" || id === "carbon-dioxide" || id === "nitrogen") {
     questionAnswerNr4 = id;
-    console.log(`Question 4 answer: ${questionAnswerNr4}`); // selected answer
     document.querySelector("#oxygen").classList.remove("selected");
     document.querySelector("#carbon-dioxide").classList.remove("selected");
     document.querySelector("#nitrogen").classList.remove("selected");
@@ -57,7 +59,6 @@ function styleSelectedAnswer(id) {
   // question 5
   else if (id === "elephant" || id === "blue-whale" || id === "girafe") {
     questionAnswerNr5 = id;
-    console.log(`Question 5 answer: ${questionAnswerNr5}`); // selected answer
     document.querySelector("#elephant").classList.remove("selected");
     document.querySelector("#blue-whale").classList.remove("selected");
     document.querySelector("#girafe").classList.remove("selected");
@@ -80,36 +81,54 @@ function checkAnswers() {
     );
     return;
   } else {
-    if (questionAnswerNr1 === correctAnswerNr1) {
-      correctAnswer(questionAnswerNr1);
+    if (questionAnswerNr1 === correctAnswersList[0]) {
+      correctAnswer(questionAnswerNr1, 0);
       result++;
     } else {
-      wrongtAnswer(questionAnswerNr1);
+      wrongAnswer(questionAnswerNr1, 0);
     }
-    if (questionAnswerNr2 === correctAnswerNr2) {
-      correctAnswer(questionAnswerNr2);
+    if (questionAnswerNr2 === correctAnswersList[1]) {
+      correctAnswer(questionAnswerNr2, 1);
       result++;
     } else {
-      wrongtAnswer(questionAnswerNr2);
+      wrongAnswer(questionAnswerNr2, 1);
     }
-    if (questionAnswerNr3 === correctAnswerNr3) {
-      correctAnswer(questionAnswerNr3);
+    if (questionAnswerNr3 === correctAnswersList[2]) {
+      correctAnswer(questionAnswerNr3, 2);
       result++;
     } else {
-      wrongtAnswer(questionAnswerNr3);
+      wrongAnswer(questionAnswerNr3, 2);
     }
-    if (questionAnswerNr4 === correctAnswerNr4) {
-      correctAnswer(questionAnswerNr4);
+    if (questionAnswerNr4 === correctAnswersList[3]) {
+      correctAnswer(questionAnswerNr4, 3);
       result++;
     } else {
-      wrongtAnswer(questionAnswerNr4);
+      wrongAnswer(questionAnswerNr4, 3);
     }
-    if (questionAnswerNr5 === correctAnswerNr5) {
-      correctAnswer(questionAnswerNr5);
+    if (questionAnswerNr5 === correctAnswersList[4]) {
+      correctAnswer(questionAnswerNr5, 4);
       result++;
     } else {
-      wrongtAnswer(questionAnswerNr5);
+      wrongAnswer(questionAnswerNr5, 4);
     }
+
+    // for
+
+    // let list = [];
+    // list[0] = questionAnswerNr1;
+    // list[1] = questionAnswerNr2;
+    // list[2] = questionAnswerNr3;
+    // list[3] = questionAnswerNr4;
+    // list[4] = questionAnswerNr5;
+    // for (let i = 0; i < list.length; i++) {
+    //   if (list[i] === correctAnswersList[i]) {
+    //     correctAnswer(list[i], i);
+    //     result++;
+    //   } else {
+    //     wrongAnswer(list[i], i);
+    //   }
+    // }
+
     // change buttons visibility
     document.querySelector(".get-answers").style.display = "none";
     document.querySelector(".restart").style.display = "block";
@@ -124,11 +143,22 @@ function restarGame() {
 }
 
 // show when answer is correct
-function correctAnswer(id) {
+// id - question selected answer's id
+// number - question number
+function correctAnswer(id, number) {
   document.querySelector(`#${id}`).classList.add("correct");
+  correctAnswerElement[number].innerText = "Your answer is correct!";
+  correctAnswerElement[number].style.display = "block";
 }
 
 // show when answer is wrong
-function wrongtAnswer(id) {
+// id - question selected answer's id
+// number - question number
+function wrongAnswer(id, number) {
   document.querySelector(`#${id}`).classList.add("wrong");
+  correctAnswerElement[number].innerText = `Incorrect, answer is ${
+    correctAnswersList[number].charAt(0).toUpperCase() +
+    correctAnswersList[number].slice(1).replace("-", " ")
+  }`;
+  correctAnswerElement[number].style.display = "block";
 }
